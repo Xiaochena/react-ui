15aaee15cba19ce2bb3a54c83bbfe8640e90dc4c
clare <clare@duomai.com>
2022-08-31 12:26:10 +0800

    fix: change prefixCls

--- src/utils/change-antd-config.tsx
+++ src/utils/change-antd-config.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { ConfigProvider } from "antd";
 import "./react-ui.css";
 
+const prefixCls = "react-ui";
 /** 高阶函数、用于链接 ConfigProvider */
 export default function changeAntdConfig<P extends object>(
   Node: React.FC<P>
 ): React.FC<P> {
-  ConfigProvider.config({ prefixCls: "chen" });
+  ConfigProvider.config({ prefixCls });
 
   return (props) => {
     return (
-      <ConfigProvider prefixCls="chen">
+      <ConfigProvider prefixCls={prefixCls}>
         <Node {...props} />
       </ConfigProvider>
     );
